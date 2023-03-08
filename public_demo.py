135fd3b56f41cf4ebe462727a9f30b94e12ff038
Mohamad-kalamouni <mouhammadkalamouni@gmail.com>
2023-03-08 15:05:53 +0200

    .

--- public_demo.py
+++ public_demo.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://bassam:ubility#07@demotest.postgres.database.azure.com:5432/postgres'
