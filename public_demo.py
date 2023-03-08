56813c3cb2cdb85660e44fc53ea27fe955568c65
Mohamad-kalamouni <mouhammadkalamouni@gmail.com>
2023-03-08 15:04:53 +0200

    Create Demo Backend

---
+++ public_demo.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://bassam:ubility#07@demotest.postgres.database.azure.com:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db= SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class persons(db.Model):
+    id= db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    address=db.Column(db.String)
+    phone=db.Column(db.Integer)
+
+    def __init__(self,name,email,address,phone):
+        self.name = name
+        self.email=email
+        self.address=address
+        self.phone=phone
+
+class User(ma.Schema):
+     class Meta:
+        fields = ("name","email","address","phone")
+
+user_schema = User()
+users_schema = User(many=True)
+
+
+
+@app.route('/get_users_data',methods=['GET'])
+def get_users():
+    all_users = persons.query.all()
+    data=users_schema.dump(all_users)
+    return jsonify(data)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
