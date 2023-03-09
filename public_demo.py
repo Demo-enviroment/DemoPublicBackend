b5c24cdf28ceb3ecedb80bf01ff6a78ccb6c9f39
Mohamad Kalamouni <mouhammadkalamouni@gmail.com>
2023-03-09 14:47:46 +0200

    add CORS

--- public_demo.py
+++ public_demo.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
 
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://bassam:ubility#07@demotest.postgres.database.azure.com:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
